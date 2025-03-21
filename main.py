a79ff79d168bf050a0dd8e556dc2f226bc78c629
rohan-shyftlabs <rohan@shyftlabs.io>
2025-03-21 09:11:50 -0400

    added more cors port

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ origins = [
     "http://localhost:8010",
     "http://127.0.0.1:8010",
     "http://localhost:8081",
-    "http://127.0.0.1:8081"
+    "http://127.0.0.1:8081",
     "http://localhost:8082",
     "http://127.0.0.1:8082"
 ]
