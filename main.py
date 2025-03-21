36cd2a8905d100cedc7ecb05b70c42bafe0636dc
rohan-shyftlabs <rohan@shyftlabs.io>
2025-03-21 08:53:06 -0400

    middle ware origins update

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8010",
-    "http://127.0.0.1:8010"
+    "http://127.0.0.1:8010",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081"
 ]
 
 app.add_middleware(
